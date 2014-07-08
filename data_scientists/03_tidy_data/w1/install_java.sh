babc3ac0645df740d125b67f3f798fc8cffad79f
annie guo <annie.guo.2011@gmail.com>
2014-07-08 23:48:29 +0000

    install java on ubuntu for R

--- data_scientists/03_tidy_data/w1/install_java.sh
+++ data_scientists/03_tidy_data/w1/install_java.sh
@@ -3,6 +3,7 @@ sudo apt-get update
 java -version
 
 sudo apt-get install default-jre
+sudo apt-get install r-cran-rjava 
 
 ##
 #https://www.digitalocean.com/community/tutorials/how-to-install-java-on-ubuntu-with-apt-get 
