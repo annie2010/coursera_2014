f725df3146273c203953a56cd89032980c97abfc
annie guo <annie.guo.2011@gmail.com>
2014-07-11 21:45:12 +0000

    import export data via R

---
+++ data_scientists/03_tidy_data/w1/install_data_table.sh
@@ -0,0 +1,23 @@
+sudo apt-get install r-cran-plyr
+sudo apt-get install r-cran-stringr
+sudo apt-get install r-cran-rcpp
+sudo apt-get install r-cran-reshape2
+sudo apt-get install r-cran-reshape2
+
+# import and export data
+# http://en.wikibooks.org/wiki/R_Programming/Importing_and_exporting_data
+
+# plyr: Tools for splitting, applying and combining data 
+# http://cran.r-project.org/web/packages/plyr/index.html
+
+# stringr: Make it easier to work with strings
+# http://cran.r-project.org/web/packages/stringr/index.html
+
+# Rcpp: Seamless R and C++ Integration 
+# http://cran.r-project.org/web/packages/Rcpp/index.html 
+
+# reshape2: Flexibly reshape data: a reboot of the reshape package 
+# http://cran.r-project.org/web/packages/reshape2/index.html
+
+# data.table: Extension of data.frame
+# http://cran.r-project.org/web/packages/data.table/index.html
