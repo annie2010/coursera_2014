2a43122426cbeab7ab9b35d4491f26502c590721
annie guo <annie.guo.2011@gmail.com>
2014-07-14 22:19:15 +0000

    installed json lite

---
+++ data_scientists/03_tidy_data/w2/install_jsonlite.R
@@ -0,0 +1,10 @@
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/mime_0.1.1.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/evaluate_0.5.5.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/formatR_0.10.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/highr_0.3.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/markdown_0.7.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/knitr_1.6.tar.gz',repo=NULL)
+install.packages('/tmp/RtmpZ3jV41/downloaded_packages/jsonlite_0.9.8.tar.gz',repo=NULL)
+
+#install.packages('jsonlite',dependecies=TRUE)
+
