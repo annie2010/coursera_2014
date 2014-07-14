f4abb9c0ac8b265078db89c4c4a4fb5a2da2466a
annie guo <annie.guo.2011@gmail.com>
2014-07-14 21:05:02 +0000

    install sqldf

---
+++ data_scientists/03_tidy_data/w2/install_sqldf.R
@@ -0,0 +1,13 @@
+#install.packages('/tmp/Rtmp7uX5lV/downloaded_packages/RJDBC_0.2-4.tar.gz',repo=NULL) # DONE (RJDBC)
+
+#install.packages('/tmp/Rtmpn14y5d/downloaded_packages/doBy_4.5-10.tar.gz',repo=NULL) #  DONE (doBy)
+#install.packages('/tmp/Rtmpn14y5d/downloaded_packages/quantreg_5.05.tar.gz',repo=NULL) # DONE (quantreg)  
+#install.packages('/tmp/Rtmpn14y5d/downloaded_packages/svUnit_0.7-12.tar.gz',repo=NULL) # DONE (svUnit)
+# > install.packages('gsubfn',dependecies=TRUE)
+
+#install.packages('/tmp/Rtmp7uX5lV/downloaded_packages/gsubfn_0.6-5.tar.gz',repo=NULL) # DONE (gsubfn)
+#install.packages('/tmp/Rtmp7uX5lV/downloaded_packages/RH2_0.1-2.12.tar.gz',repo=NULL) # DONE (RH2)
+#install.packages('/tmp/Rtmp7uX5lV/downloaded_packages/RSQLite.extfuns_0.0.1.tar.gz',repo=NULL) # DONE (RSQLite.extfuns)
+
+install.packages('/tmp/Rtmp7uX5lV/downloaded_packages/sqldf_0.4-7.1.tar.gz',repo=NULL) # DONE (sqldf)
+# > install.packages('sqlfd',dependecies=TRUE)
