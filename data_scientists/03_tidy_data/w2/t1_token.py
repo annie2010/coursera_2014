fd1a780935631c59d5ddfd7cb2531af62375a064
annie guo <annie.guo.2011@gmail.com>
2014-07-14 23:12:35 +0000

    github auth via token

---
+++ data_scientists/03_tidy_data/w2/t1_token.py
@@ -0,0 +1,26 @@
+import urllib2
+import json
+from StringIO import StringIO
+import base64
+
+auth_method="Token "
+token = TBD
+auth = auth_method + token
+
+req1 = urllib2.Request("https://api.github.com/users/jtleek/repos")
+req = urllib2.Request("https://api.github.com/repos/jtleek/datasharing")
+req.add_header("Authorization", auth)
+req.add_header("Content-Type", "application/json")
+req.add_header("Accept", "application/json")
+res = urllib2.urlopen(req)
+
+data = res.read()
+repository = json.load(StringIO(data))
+print repository['created_at']
+
+# http://stackoverflow.com/questions/6371032/how-to-get-github-token-using-username-and-password
+# curl -u <token>:x-oauth-basic https://api.github.com/user
+
+# result - 2013-11-07T13:25:07Z
+
+
