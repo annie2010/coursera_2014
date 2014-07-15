9a6f4ecffbce23f935502ba2a2011ee82fb94269
annie guo <annie.guo.2011@gmail.com>
2014-07-15 05:56:41 +0000

    install quantmod

---
+++ data_scientists/03_tidy_data/w4/install_quantmod.R
@@ -0,0 +1,4 @@
+install.packages('/tmp/RtmpYj2qQx/downloaded_packages/Defaults_1.1-1.tar.gz', repo=NULL) # DONE (Defaults)
+install.packages('/tmp/RtmpYj2qQx/downloaded_packages/TTR_0.22-0.tar.gz', repo=NULL) # DONE (TTR)
+install.packages('/tmp/RtmpYj2qQx/downloaded_packages/quantmod_0.4-0.tar.gz', repo=NULL) # DONE (quantmod)
+
