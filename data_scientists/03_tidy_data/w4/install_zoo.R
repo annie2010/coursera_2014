09cb8ad778b815a254d3de66ca809e63b3863524
annie guo <annie.guo.2011@gmail.com>
2014-07-15 05:26:31 +0000

    install zoo

---
+++ data_scientists/03_tidy_data/w4/install_zoo.R
@@ -0,0 +1,8 @@
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/BH_1.54.0-2.tar.gz', repo=NULL) # DONE (BH)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/lattice_0.20-29.tar.gz', repo=NULL) #  DONE (lattice)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/DAAG_1.20.tar.gz', repo=NULL) # DONE (DAAG)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/fts_0.9.9.tar.gz', repo=NULL) # DONE (fts)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/mondate_0.10.01.02.tar.gz', repo=NULL) # DONE (mondate)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/tis_1.23.tar.gz', repo=NULL) #  DONE (tis)
+# install.packages('/tmp/RtmpSp62oB/downloaded_packages/zoo_1.7-11.tar.gz', repo=NULL) # DONE (zoo)
+
